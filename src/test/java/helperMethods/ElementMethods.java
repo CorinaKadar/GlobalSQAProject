6dd04991584d9d09a9e7955b9760545e6afc6edf
CorinaKadar <corina.jula@yahoo.com>
2024-09-08 14:31:21 +0300

    Update for CI/CD

--- src/test/java/helperMethods/ElementMethods.java
+++ src/test/java/helperMethods/ElementMethods.java
@@ -21,7 +21,7 @@ public class ElementMethods {
     public WebDriver driver;
 
     public void waitForElementToBeClickable(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
