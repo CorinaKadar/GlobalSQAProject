381ebfae93ade168c4a32d652b7897801d5677a4
CorinaKadar <corina.jula@yahoo.com>
2024-09-12 23:11:26 +0300

    Log check

--- src/test/java/pages/BankCustomerTransactionsListPage.java
+++ src/test/java/pages/BankCustomerTransactionsListPage.java
@@ -74,8 +74,9 @@ public class BankCustomerTransactionsListPage extends BasePage {
         String extractedXPath = elementMethods.getFindByAnnotationValue(this, "transactionsTableRowValues");
         LoggerUtility.info("Successfully extracted the xpath value.");
         transactionsTableRowValues = elementMethods.refreshTransactionTableRowValues(extractedXPath);
-        LoggerUtility.info("After refreshing the transactionTableRowValues the size is: " + transactionsTableRowValues.size());
+
         try {
+            LoggerUtility.info("After refreshing the transactionTableRowValues the size is: " + transactionsTableRowValues.size());
             for (Integer i = 0; i < transactionsTableRowValues.size(); i++) {
                 String actualDateTime = columnDateTimeValues.get(i).getText().trim();
                 String actualAmount = columnAmountValues.get(i).getText().trim();
