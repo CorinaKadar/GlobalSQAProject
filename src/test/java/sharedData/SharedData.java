e6c1dac677d627ba0945ba06bda8bdaff5c6e6eb
CorinaKadar <corina.jula@yahoo.com>
2024-09-14 13:16:52 +0300

    Minor Update

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -3,8 +3,8 @@ package sharedData;
 import loggerUtility.LoggerUtility;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,11 +17,11 @@ public class SharedData {
 
     @BeforeMethod
     public void prepareEnv() {
-        FirefoxOptions options = new FirefoxOptions();
+        EdgeOptions options = new EdgeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--disable-search-engine-choice-screen");
         options.addArguments("window-size=1920x1080");
-        driver = new FirefoxDriver(options);
+        driver = new EdgeDriver(options);
         LoggerUtility.info("The browser is successfully opened.");
         driver.get("https://www.globalsqa.com/");
         driver.manage().window().maximize();
